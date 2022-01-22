31d4a2bdc708a7f80b1c9787e9ed151fc6c49122
Kirill LS <lamp2502@mail.ru>
2022-01-22 23:01:07 +0300

    commit_1

---
+++ src/main/java/ru/lamp2502/jmixjavarush/screen/user/UserBrowse.java
@@ -0,0 +1,12 @@
+package ru.lamp2502.jmixjavarush.screen.user;
+
+import ru.lamp2502.jmixjavarush.entity.User;
+import io.jmix.ui.navigation.Route;
+import io.jmix.ui.screen.*;
+
+@UiController("User.browse")
+@UiDescriptor("user-browse.xml")
+@LookupComponent("usersTable")
+@Route("users")
+public class UserBrowse extends StandardLookup<User> {
+}
---
+++ src/test/java/ru/lamp2502/jmixjavarush/JmixjavarushApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lamp2502.jmixjavarush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmixjavarushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
